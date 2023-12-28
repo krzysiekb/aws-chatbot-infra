d132281fae9603050ec75c9c147cfd1c7594a438
Krzysztof Banasiak <krzysztof.banasiak@gmail.com>
2023-12-28 16:41:04 +0100

    feat: Add lambda dependencies

--- backend/backend_stack.py
+++ backend/backend_stack.py
@@ -1,4 +1,5 @@
-from aws_cdk import Stack, aws_lambda, aws_apigateway, aws_iam
+from aws_cdk import Stack, aws_lambda, aws_apigateway, aws_iam, RemovalPolicy
+from aws_cdk.aws_lambda_python_alpha import PythonLayerVersion
 from constructs import Construct
 
 
@@ -6,12 +7,23 @@ class BackendStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
+        backend_lambda_dependencies = PythonLayerVersion(
+            scope=self,
+            id="ChatbotBackendLambdaDependencies",
+            entry="backend/lambda_dependencies",
+            compatible_runtimes=[aws_lambda.Runtime.PYTHON_3_11],
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
         backend_lambda = aws_lambda.Function(
             scope=self,
             id="ChatbotBackendLambda",
             runtime=aws_lambda.Runtime.PYTHON_3_11,
             handler="backend_lambda.handler",
             code=aws_lambda.Code.from_asset("backend/lambda"),
+            layers=[
+                backend_lambda_dependencies
+            ]
         )
 
         backend_lambda.add_to_role_policy(
